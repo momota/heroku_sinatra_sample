b77b9d00ce6289b9ebf0c26f3286fd291af39daa
momota <makoto.momota@gmail.com>
2013-09-16 16:45:53 +0900

    4th lesson

--- main.rb
+++ main.rb
@@ -1,11 +1,26 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+=begin
 get '/' do
   "hello world"
 end
 
 get '/about' do
   "about this site"
+end
+
+get '/hello/:name' do
+  "hello #{params[:name]}"
+end
+
+get '/hello/:name' do |n|
+  "hello #{n}"
+end
+
+=end
+
+get '/hello/:fname/?:lname?' do |f, l|
+  "hello #{f} #{l}"
 end
 
