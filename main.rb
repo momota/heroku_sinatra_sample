31441e13035cce87754200d2fd88eefd7e8676ef
momota <makoto.momota@gmail.com>
2013-09-16 23:30:56 +0900

    14th lesson

--- main.rb
+++ main.rb
@@ -1,40 +1,23 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'active_record'
 
-=begin
+
+ActiveRecord::Base.establish_connection(
+        "adapter"  => "sqlite3",
+        "database" => "./bbs.db"
+)
+
+class Comment < ActiveRecord::Base
+end
+
 get '/' do
-  "hello world"
-end
-
-get '/about' do
-  "about this site"
-end
-
-get '/hello/:name' do
-  "hello #{params[:name]}"
-end
-
-get '/hello/:name' do |n|
-  "hello #{n}"
-end
-
-get '/hello/:fname/?:lname?' do |f, l|
-  "hello #{f} #{l}"
-end
-
-get '/from/*/to/*' do |f, t|
-  "from #{f} to #{t}"
-end
-
-get %r{/users/([0-9]+)} do |i|
-  "user id = #{i}"
-end
-
-=end
-
-get '/:name' do |n|
-  @name  = n
-  @title = 'sinatra sample'
+  @comments = Comment.order("id desc").all
   erb :index
+end
+
+post '/new' do
+  Comment.create({ :body => params[:body] })
+  redirect '/'
 end
 
