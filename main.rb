163ba280970916ac3440351fa784740d469b263a
momota <makoto.momota@gmail.com>
2013-09-16 17:36:12 +0900

    5th lesson

--- main.rb
+++ main.rb
@@ -18,9 +18,23 @@ get '/hello/:name' do |n|
   "hello #{n}"
 end
 
-=end
-
 get '/hello/:fname/?:lname?' do |f, l|
   "hello #{f} #{l}"
+end
+
+get '/from/*/to/*' do |f, t|
+  "from #{f} to #{t}"
+end
+
+get %r{/users/([0-9]+)} do |i|
+  "user id = #{i}"
+end
+
+=end
+
+get '/:name' do |n|
+  @name  = n
+  @title = 'sinatra sample'
+  erb :index
 end
 
