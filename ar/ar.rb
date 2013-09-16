f1d72e361ee5ee66d7321b0662b4fbc4b4a77dd1
momota <makoto.momota@gmail.com>
2013-09-17 00:00:39 +0900

    ar 4th lesson

--- ar/ar.rb
+++ ar/ar.rb
@@ -8,7 +8,23 @@ ActiveRecord::Base.establish_connection(
 class Post < ActiveRecord::Base
 end
 
+=begin
 post = Post.new(:title => "title1", :body => "body1")
 post.save
+=end
+
+post2 = Post.new
+post2.title = "title2"
+post2.body  = "body2"
+post2.save
+
+post3 = Post.new do |p|
+  p.title = "title3"
+  p.body  = "body3"
+end
+post3.save
+
+Post.create(:title => "title4", :body => "body4")
+
 
 p Post.all
